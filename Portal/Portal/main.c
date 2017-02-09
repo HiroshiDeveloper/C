0768a8b618d3e2a70d82e5b4eba828d3ae843afd
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2017-02-09 10:02:58 -0800

    Portal

--- Portal/Portal/main.c
+++ Portal/Portal/main.c
@@ -13,8 +13,11 @@
 #define ACCOUNTSPATH "/Users/hiroshi/Documents/Github/C/Portal/Portal/Accounts.txt"
 #define STUDENTSPATH "/Users/hiroshi/Documents/Github/C/Portal/Portal/Students.txt"
 
+#define CHARLENGTH 256
+
 void loginMenu();
-void file(char *fileName, int storeSize);
+void AccountFile(char *fileName, char *studentId, char *password);
+char *extractWords(char *line);
 
 int main(int argc, const char * argv[]) {
     loginMenu();
@@ -23,29 +26,30 @@ int main(int argc, const char * argv[]) {
 }
 
 void loginMenu(){
-    char studentId;
-    char password;
+    char *studentId = malloc(sizeof(char) * CHARLENGTH);
+    char *password = malloc(sizeof(char) * CHARLENGTH);
     int storeSize = 0;
     
     printf("******************************************\n");
     printf("Please enter your account to login:\n");
     printf("******************************************\n");
     printf("StudentId: ");
-    scanf("%s", &studentId);
+    scanf("%s", studentId);
     printf("Password: ");
-    scanf("%s", &password);
+    scanf("%s", password);
     
     storeSize = 2;
-    file(ACCOUNTSPATH, storeSize);
+    AccountFile(ACCOUNTSPATH, studentId, password);
+    free(studentId);
+    free(password);
 }
 
-void file(char *fileName, int storeSize){
+void AccountFile(char *fileName, char *studentId, char *password){
     FILE *fp;
-    char *line = NULL;
+    char *line = NULL, *words;
     size_t len = 0;
     ssize_t read;
-    int flg = 0;
-    int length = 0;
+    int idFlg = 0;
     
     // file open
     fp=fopen(fileName, "r");
@@ -56,23 +60,34 @@ void file(char *fileName, int storeSize){
         exit(0);
     }
     
+    printf("%s\n", studentId);
+    
     // read the file line by line
     while ((read = getline(&line, &len, fp)) != -1) {
-        //printf("Retrieved line of length %zu :\n", read);
-        //printf("%s", line);
-        flg = 0;
-        length = (int)(strlen(line) - 2);
-        for(int i = 0; i < length; i++){
-            if(flg == 1){
-                printf("%c ", line[i]);
-            }else if(line[i] == '\"'){
-                flg = 1;
+        if((int)strlen(line) != 1){
+            words = extractWords(line);
+            if(strcmp(words, studentId) == 0){
+                idFlg = 1;
+            }else if(idFlg == 1 && strcmp(words, password) == 0){
+                printf("WELCOME\n");
             }
+        }else{
+            idFlg = 0;
         }
-        printf("\n");
     }
 }
 
-//void check(){
-//
-//}
+char *extractWords(char *line){
+    int length = (int)(strlen(line) - 2);   // subtract '"' + '\n'
+    int checkPoint = 0;
+    char words[length];
+    char *result;
+    
+    for(int i = 0; i < length; i++){
+        if(line[i] == '\"'){
+            checkPoint = i;
+        }
+    }
+    result = memcpy(words, &line[checkPoint + 1], length - checkPoint - 1);
+    return result;
+}
