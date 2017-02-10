4c70050b9cbe83d89c46114aa0a6eb5b70f7ddef
HiroshiDeveloper <19.hiroshi.13@gmail.com>
2017-02-10 07:21:00 -0800

    devinding files

--- Portal/Portal/main.c
+++ Portal/Portal/main.c
@@ -6,89 +6,52 @@
 //  Copyright © 2017 Hiroshi. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "header.h"
+#include "accounts.h"
 
-#define ACCOUNTSPATH "/Users/hiroshi/Documents/Github/C/Portal/Portal/Accounts.txt"
-#define STUDENTSPATH "/Users/hiroshi/Documents/Github/C/Portal/Portal/Students.txt"
+// account
+struct account *loginMenu(struct account *account, char *studentId, char *password);
 
-#define CHARLENGTH 256
-
-void loginMenu();
-void AccountFile(char *fileName, char *studentId, char *password);
-char *extractWords(char *line);
+// main
+int *mainMenu();
 
 int main(int argc, const char * argv[]) {
-    loginMenu();
+    char *studentId = malloc(sizeof(char) * CHARLENGTH);
+    char *password = malloc(sizeof(char) * CHARLENGTH);
     
+    // Account check
+    struct account *account = (struct account*)malloc(sizeof(struct account));
+    account = loginMenu(account, studentId, password);
+    printf("StudentId : %s\n", account->studentId);
+    printf("Password : %s\n", account->password);
+    
+    // Main menu
+    
+    
+    free(account);
+    free(studentId);
+    free(password);
     return 0;
 }
 
-void loginMenu(){
-    char *studentId = malloc(sizeof(char) * CHARLENGTH);
-    char *password = malloc(sizeof(char) * CHARLENGTH);
-    int storeSize = 0;
+int *mainMenu(){
+    int *number;
     
-    printf("******************************************\n");
-    printf("Please enter your account to login:\n");
-    printf("******************************************\n");
-    printf("StudentId: ");
-    scanf("%s", studentId);
-    printf("Password: ");
-    scanf("%s", password);
+    printf("******************************************************\n");
+    printf("Select from the optiosn:\n");
+    printf("******************************************************\n");
+    printf("-[1] Print my enrolment certificate\n");
+    printf("-[2] Print my courses\n");
+    printf("-[3] Print my transcript\n");
+    printf("-[4] Print my GPA\n");
+    printf("-[5] Print my ranking among all studednts in the college\n");
+    printf("-[6] List all available courses\n");
+    printf("-[7] List all students\n");
+    printf("-[8] Logout\n");
+    printf("-[9] Exit\n");
+    printf("******************************************************\n");
+    printf("Enter the number corresponding to each item to procees:\n");
+    scanf("%d", number);
     
-    storeSize = 2;
-    AccountFile(ACCOUNTSPATH, studentId, password);
-    free(studentId);
-    free(password);
-}
-
-void AccountFile(char *fileName, char *studentId, char *password){
-    FILE *fp;
-    size_t len = 0;
-    ssize_t read;
-    char *line = NULL, *words;
-    int idFlg = 0;
-    
-    // file open
-    fp=fopen(fileName, "r");
-    
-    // error if the file is not found
-    if (fp == NULL){
-        printf("Cannot find the text file in this URL: %s\n", fileName);
-        exit(0);
-    }
-    
-    // read the file line by line
-    while ((read = getline(&line, &len, fp)) != -1) {
-        if((int)strlen(line) != 1){
-            words = extractWords(line);             // extract words e.g.) StudentId, Pass
-            if(strcmp(words, studentId) == 0){
-                idFlg = 1;
-            }else if(idFlg == 1 && strcmp(words, password) == 0){   // check studentId and pass
-                printf("WELCOME\n");
-            }
-            free(words);
-        }else{
-            idFlg = 0;
-        }
-    }
-}
-
-char *extractWords(char *line){
-    int length = (int)(strlen(line) - 2);   // subtract '"' + '\n'
-    int checkPoint = -1;
-    char *words = malloc(sizeof(char) * length);
-    
-    for(int i = 0; i < length; i++){
-        
-        if(checkPoint != -1){
-            words[checkPoint] = line[i];    // copy characters one by one
-            checkPoint++;
-        }else if(line[i] == '\"'){          // find this character '"'
-            checkPoint = 0;
-        }
-    }
-    return words;
+    return number;
 }
